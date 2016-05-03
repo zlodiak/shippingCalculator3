43d73f7ea814c3422b1ce6fead3a3236b9c20d44
zlodiak <prozaik81-2@yandex.ru>
2016-05-03 14:35:52 +0300

    weight check ok

--- js/project/models/models.js
+++ js/project/models/models.js
@@ -14,6 +14,21 @@ APP.CalculatorModel = Backbone.Model.extend({
         errorsShippingOptionsWeight = [],
         errorsShippingOptionsVolume = [];
 
+    var emptyCheckDepartureCity = this.emptyCheck(attrs.departureCity), 
+        isNumCheckDepartureCity = this.isNumCheck(attrs.departureCity);
+
+    if(emptyCheckDepartureCity) { errorsDepartureCity.push(emptyCheckDepartureCity) };
+    if(isNumCheckDepartureCity) { errorsDepartureCity.push(isNumCheckDepartureCity) };
+
+    var emptyCheckShippingOptionsWeight = this.emptyCheck(attrs.shippingOptionsWeight), 
+        negativeNumShippingOptionsWeight = this.negativeNumCheck(attrs.shippingOptionsWeight), 
+        nullNumShippingOptionsWeight = this.nullNumCheck(attrs.shippingOptionsWeight), 
+        isStrCheckShippingOptionsWeight = this.isStrCheck(attrs.shippingOptionsWeight);
+
+    if(emptyCheckShippingOptionsWeight) { errorsShippingOptionsWeight.push(emptyCheckShippingOptionsWeight) };
+    if(negativeNumShippingOptionsWeight) { errorsShippingOptionsWeight.push(negativeNumShippingOptionsWeight) };
+    if(nullNumShippingOptionsWeight) { errorsShippingOptionsWeight.push(nullNumShippingOptionsWeight) };
+    if(isStrCheckShippingOptionsWeight) { errorsShippingOptionsWeight.push(isStrCheckShippingOptionsWeight) };
 
     if(
       errorsDepartureCity.length != 0 ||
@@ -28,6 +43,26 @@ APP.CalculatorModel = Backbone.Model.extend({
         'shippingOptionsVolume': errorsShippingOptionsVolume
       };
     };
-  }  
+  },
+
+  emptyCheck: function(value) { 
+    if($.trim(value).length == 0) { return 'Это поле не может быть пустым' };
+  },
+
+  negativeNumCheck: function(value) { 
+    if(parseInt($.trim(value), 10) < 0) { return 'Это значение не может быть меньше нуля' };
+  },
+
+  nullNumCheck: function(value) { 
+    if(parseInt($.trim(value), 10) == 0) { return 'Это значение не может быть равно нулю' };
+  },
+
+  isNumCheck: function(value) { 
+    if(isNaN(parseInt($.trim(value), 10)) == false) { return 'Это значение не может быть цифрой' };
+  },
+
+  isStrCheck: function(value) { 
+    if(isNaN(parseInt($.trim(value), 10)) == true) { return 'Это значение должно быть цифрой' };
+  }            
 });
 
