b6441c43cc2a271902c889b6d9b1b44c67d950b1
zlodiak <prozaik81-2@yandex.ru>
2016-05-04 13:34:16 +0300

    bootstrap ok

--- templates.php
+++ templates.php
@@ -1,12 +1,12 @@
 <script type="text/tpl" id="calcTpl">
-  <div class="cont">
+  <div class="container">
     <div class="row">
       <div class="col-xs-12"><h1>Калькулятор доставки</h1></div>
     </div>
 
     <div class="row">
-      <div class="col-xs-6" id="departCityWidgetCont"></div>
-      <div class="col-xs-6" id="destinCityWidgetCont"></div>
+      <div class="col-xs-12 col-sm-6" id="departCityWidgetCont"></div>
+      <div class="col-xs-12 col-sm-6" id="destinCityWidgetCont"></div>
     </div> 
 
     <div class="row">
