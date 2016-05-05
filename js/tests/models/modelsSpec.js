8834a1660436ea9b80e29706a74026d1258aea7e
zlodiak <prozaik81-2@yandex.ru>
2016-05-05 15:14:14 +0300

    model rset ok

--- js/tests/models/modelsSpec.js
+++ js/tests/models/modelsSpec.js
@@ -13,4 +13,37 @@ describe("DepartCityModel", function() {
   it("should is defined", function() {
     expect(this.model).toBeDefined();
   });
+
+  it("should errDepartCity is not empty after init", function() {
+    var ModelcountElem = this.model.get('errDepartCity').length,
+        DATAcountElem = DATA.errDepartCity.length;
+
+    expect(ModelcountElem).toEqual(DATAcountElem)
+  });  
+
+  it("should errDepartCity is empty after _resetErr()", function() {
+    this.model._resetErr();
+
+    var ModelcountElem = this.model.get('errDepartCity').length,
+        DATAcountElem = DATA.errDepartCity.length;    
+
+    expect(ModelcountElem).toEqual(0)
+  });   
+
+  it("should all errors props is empty after _resetErr()", function() {
+    this.model._resetErr();
+
+    var ModelcountElems = this.model.get('errDepartCity').length + 
+                          this.model.get('errDestinCity').length +
+                          this.model.get('errShippOptionsWeight').length +
+                          this.model.get('errShippOptionsVolume').length;
+
+    var DATAcountElem = DATA.errDepartCity.length;    
+
+    expect(ModelcountElems).toEqual(0)
+  });    
 });
+
+
+
+//_resetErr
