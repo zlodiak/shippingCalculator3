966341c5cfc821a497e8ff9d076a0013122a8164
zlodiak <prozaik81-2@yandex.ru>
2016-05-05 10:27:47 +0300

    isnum check ok

--- js/tests/valuesValidatorSpec.js
+++ js/tests/valuesValidatorSpec.js
@@ -10,6 +10,7 @@ describe("emptyCheck", function() {
   });  
 });
 
+
 describe("minusNumCheck", function() {
   var errorMessage = 'Это значение не может быть меньше нуля';
 
@@ -26,6 +27,7 @@ describe("minusNumCheck", function() {
   });    
 });
 
+
 describe("nullNumCheck", function() {
   var errorMessage = 'Это значение не может быть равно нулю';
 
@@ -40,4 +42,29 @@ describe("nullNumCheck", function() {
   it("should not return errormessage for negative value", function() {
     expect(APP.valuesValidator.nullNumCheck(-55)).not.toEqual(errorMessage);
   });       
+});
+
+
+describe("isNumCheck", function() {
+  var errorMessage = 'Это значение не может быть цифрой';
+
+  it("should return errormessage for number", function() {
+    expect(APP.valuesValidator.isNumCheck(444)).toEqual(errorMessage);
+  });
+
+  it("should return errormessage for float", function() {
+    expect(APP.valuesValidator.isNumCheck(444.45)).toEqual(errorMessage);
+  });  
+
+  it("should not return errormessage for string", function() {
+    expect(APP.valuesValidator.isNumCheck('sdgs')).not.toEqual(errorMessage);
+  }); 
+
+  it("should not return errormessage for boolean", function() {
+    expect(APP.valuesValidator.isNumCheck(true)).not.toEqual(errorMessage);
+  });  
+
+  it("should not return errormessage for undefined", function() {
+    expect(APP.valuesValidator.isNumCheck(undefined)).not.toEqual(errorMessage);
+  });         
 });
